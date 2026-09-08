47f6b654f8fefd4af042de4d60c13cb0d19a7f7e
RileyBerry7 <rileyberry7@gmail.com>
2026-09-08 15:26:43 -0700

    Fix: comilation script. Implemented hittable class into main, rendered floor plane.

--- main.cpp
+++ main.cpp
@@ -1,38 +1,18 @@
-#include "color.h"
-#include "ray.h"
-#include "vec3.h"
+#include "rtweekend.h"
 
-#include <iostream>
+#include "hittable.h"
+#include "hittable_list.h"
+#include "sphere.h"
 
-double hit_sphere(const point3& center, double radius, const ray& r) {
-    vec3 oc = center - r.origin();
-    auto a = r.direction().length_squared();
-    auto h = dot(oc, r.direction());
-    auto c = oc.length_squared() - radius*radius;
-    auto discriminant = h*h - a*c;
-    
-    if (discriminant < 0) return -1.0;
-    auto t = (h - std::sqrt(discriminant)) / a;
-    return t;
-    
-}
 
-color ray_color(const ray& r) {
-    auto center = point3(0,0,-1);
-    auto t = hit_sphere(center, 0.5, r);
-    if (t > 0.0) {
-        auto normal = unit_vector(r.at(t) - center);
-        return 0.5 * (normal + color(1,1,1));
+color ray_color(const ray& r, const hittable& world) {
+    // Sphere
+    hit_record rec;
+    if (world.hit(r, 0, infinity, rec)) {
+        return 0.5 * (rec.normal + color(1, 1, 2));
     }
 
-    center = point3(-2.1, 0.0,-1.8);
-    t = hit_sphere(center, 0.5, r);
-    if (t > 0.0) {
-        auto normal = unit_vector(r.at(t) - center);
-        return 0.5 * (normal + color(1,1,1));
-    }
-
-
+    // Background
     vec3 unit_direction = unit_vector(r.direction());
     auto a = 0.5*(unit_direction.y() + 1.0);
     return (1.0-a)*color(1.0, 1.0, 1.0) + a*color(0.5, 0.7, 1.0);
@@ -45,6 +25,12 @@ int main() {
     int image_width    = 400;
     int image_height   = int(image_width / aspect_ratio);
     image_height = (image_height < 1) ? 1 : image_height; // Must be greater than 0
+
+    // World
+    hittable_list world;
+    world.add(make_shared<sphere>(point3(0,0,-1), 0.5));
+    world.add(make_shared<sphere>(point3(0,-100.5,-1), 100));
+
     
     // Camera
     auto focal_length = 1.0;
@@ -74,7 +60,7 @@ int main() {
             auto pixel_center  = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
             auto ray_direction = pixel_center - camera_center;
             ray r(camera_center, ray_direction);
-            color pixel_color  = ray_color(r);  
+            color pixel_color  = ray_color(r, world);  
 
             write_color(std::cout, pixel_color);
         }
