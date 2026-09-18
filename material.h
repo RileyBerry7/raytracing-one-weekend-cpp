1a479a98d38ed79fdcfd7d79bcb9b084d3bad188
RileyBerry7 <rileyberry7@gmail.com>
2026-09-17 18:50:41 -0700

    mat: implement dielectric material

--- material.h
+++ material.h
@@ -78,6 +78,35 @@ private:
     double fuzz;
 };
 
+
+//=======================================================================================================
+// GLASS (TRANSPARENT)
+class dielectric : public material {
+public:
+    // Constructor
+    dielectric(double refraction_index) : refraction_index(refraction_index) {}
+
+    //***************************************************************************************************
+    // DIALECTRIC SCATTER
+    virtual bool scatter(const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered) const override {
+
+        attenuation = color(1.0, 1.0, 1.0);
+        double ri   = rec.front_face ? (1.0/refraction_index) : refraction_index;
+
+        vec3 unit_direction = unit_vector(r_in.direction());
+        vec3 refracted      = refract(unit_direction, rec.nornal, ri);
+
+        // Save scattered ray
+        scattered = ray(rec.p, refractred);
+
+        // Return reflection status
+        return true; //  Always true
+    }
+private:
+    double refraction_index; // may be a ration of ri/ri of enclosing medium
+};
+
+
 //--------------------------------------------------------------------------------
 #endif
 
